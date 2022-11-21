453b80a30a0e80b9a1d0e597e45eb74f5c2539a8
Mihai Gavrilescu <mihai.gavrilescu@yahoo.com>
2022-11-21 17:20:40 +0200

    PCMS-16: Added generic repositories.

---
+++ src/main/java/com/sda/mihai/petclinicmanagement/repository/BaseRepository.java
@@ -0,0 +1,16 @@
+package com.sda.mihai.petclinicmanagement.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BaseRepository<T> {
+    Optional<T> findById(Integer id);
+
+    void create(T entity);
+
+    void update(T entity);
+
+    void delete(T entity);
+
+    List<T> findALl();
+}
---
+++ src/main/java/com/sda/mihai/petclinicmanagement/repository/BaseRepositoryImpl.java
@@ -0,0 +1,102 @@
+package com.sda.mihai.petclinicmanagement.repository;
+
+import com.sda.mihai.petclinicmanagement.utils.SessionManager;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class BaseRepositoryImpl<T> implements BaseRepository<T> {
+    private Class<T> entityClass;
+
+    public BaseRepositoryImpl(Class<T> entityClass) {
+        this.entityClass = entityClass;
+    }
+
+    public Optional<T> findById(Integer id) {
+        try (Session session = SessionManager.getSessionFactory().openSession()) {
+            T entity = session.find(entityClass, id);
+            return Optional.ofNullable(entity);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public void create(T entity) {
+        Transaction transaction = null;
+
+        try (Session session = SessionManager.getSessionFactory().openSession()) {
+
+            transaction = session.beginTransaction();
+
+            session.save(entity);
+
+            transaction.commit();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
+    }
+
+    @Override
+    public void update(T entity) {
+        Transaction transaction = null;
+
+        try (Session session = SessionManager.getSessionFactory().openSession()) {
+
+            transaction = session.beginTransaction();
+
+            session.update(entity);
+
+            transaction.commit();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
+
+    }
+
+    @Override
+    public void delete(T entity) {
+        Transaction transaction = null;
+
+        try (Session session = SessionManager.getSessionFactory().openSession()) {
+
+            transaction = session.beginTransaction();
+
+            session.delete(entity);
+
+            transaction.commit();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
+
+    }
+
+    @Override
+    public List<T> findALl() {
+        try (Session session = SessionManager.getSessionFactory().openSession()) {
+            return session.createQuery("FROM " + entityClass.getName(), entityClass).list();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ArrayList<>();
+        }
+    }
+}
